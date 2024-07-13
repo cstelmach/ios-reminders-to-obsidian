e894da40c665a2c907d0ed440cf71979dcd87cfa
cstelmach <christian.stel@hotmail.com>
2024-07-13 07:06:51 +0200

    feat: get reminders db automatically by size

--- reminders.py
+++ reminders.py
@@ -2,55 +2,85 @@
 
 from Foundation import NSDate
 from EventKit import EKEventStore, EKReminder, EKEntityTypeReminder
+import os
+import json
+import glob
 import sqlite3
+
 
 def get_all_reminder_lists():
     store = EKEventStore.alloc().init()
     store.requestAccessToEntityType_completion_(EKEntityTypeReminder, None)
     calendars = store.calendarsForEntityType_(EKEntityTypeReminder)
-    
+
     reminder_lists = []
     for calendar in calendars:
-        reminder_lists.append({
-            'title': calendar.title(),
-            'color': calendar.color(),
-            'type': calendar.type(),
-            'allowsContentModifications': calendar.allowsContentModifications(),
-            'UUID': str(calendar.calendarIdentifier())
-        })
+        reminder_lists.append(
+            {
+                "title": calendar.title(),
+                "color": calendar.color(),
+                "type": calendar.type(),
+                "allowsContentModifications": calendar.allowsContentModifications(),
+                "UUID": str(calendar.calendarIdentifier()),
+            }
+        )
     return reminder_lists
+
 
 def get_completed_reminders_for_list(list_name):
     store = EKEventStore.alloc().init()
     store.requestAccessToEntityType_completion_(EKEntityTypeReminder, None)
     calendars = store.calendarsForEntityType_(EKEntityTypeReminder)
-    
-    target_calendar = next((calendar for calendar in calendars if calendar.title() == list_name), None)
+
+    target_calendar = next(
+        (calendar for calendar in calendars if calendar.title() == list_name), None
+    )
     if not target_calendar:
         print(f"List '{list_name}' not found.")
         return []
-    
+
     predicate = store.predicateForRemindersInCalendars_([target_calendar])
     reminders = store.remindersMatchingPredicate_(predicate)
-    
+
     completed_reminders = []
     for reminder in reminders:
         if reminder.isCompleted():
-            completed_reminders.append({
-                'name': reminder.title(),
-                'body': reminder.notes(),
-                'creationDate': reminder.creationDate().description(),
-                'completionDate': reminder.completionDate().description() if reminder.completionDate() else None,
-                'allDayDueDate': reminder.dueDateAllDay(),
-                'dueDate': reminder.dueDate().description() if reminder.dueDate() else None,
-                'priority': reminder.priority(),
-                'UUID': reminder.UUID()
-            })
-    
+            completed_reminders.append(
+                {
+                    "name": reminder.title(),
+                    "body": reminder.notes(),
+                    "creationDate": reminder.creationDate().description(),
+                    "completionDate": (
+                        reminder.completionDate().description()
+                        if reminder.completionDate()
+                        else None
+                    ),
+                    "allDayDueDate": reminder.dueDateAllDay(),
+                    "dueDate": (
+                        reminder.dueDate().description() if reminder.dueDate() else None
+                    ),
+                    "priority": reminder.priority(),
+                    "UUID": reminder.UUID(),
+                }
+            )
+
     return completed_reminders
 
+
 def find_parent_reminder(reminder_uuid):
-    db_path = "/Users/cs/Library/Group Containers/group.com.apple.reminders/Container_v1/Stores/Data-FEC9D2EC-9C03-4EE5-A85C-AD1172799AC7.sqlite"
+    # get the path to the reminders database
+    db_glob_path = os.path.expanduser(
+        "~/Library/Group Containers/group.com.apple.reminders/Container_v1/Stores/Data-*.sqlite"
+    )
+    db_paths = glob.glob(db_glob_path)
+
+    if not db_paths:
+        return json.dumps({"error": "Reminders database not found"})
+
+    # find out the largest of the databases
+    db_paths.sort(key=lambda x: os.path.getsize(x), reverse=True)
+    db_path = db_paths[0]  # Use the first matching database file
+
     conn = None
     try:
         conn = sqlite3.connect(db_path)
@@ -64,7 +94,7 @@ def find_parent_reminder(reminder_uuid):
         cursor.execute(query, (reminder_uuid,))
         result = cursor.fetchone()
         if result:
-            return {'uuid': result[0], 'title': result[1]}
+            return {"uuid": result[0], "title": result[1]}
         else:
             return None
     except sqlite3.Error as e:
