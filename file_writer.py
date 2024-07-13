d1f48a5826583ef3dba5347168e461ada8dc8ba5
cstelmach <christian.stel@hotmail.com>
2024-07-13 16:54:37 +0200

    feat: use markdown template for non-existing days

--- file_writer.py
+++ file_writer.py
@@ -1,3 +1,5 @@
+import os
+import json
 from config import config
 from datetime_formatter import format_date_for_filename
 from header_checker import check_section_header_exists
@@ -10,12 +12,12 @@ def write_reminders_to_markdown(reminder_list, completed_reminders):
     folder_path = (
         config["dailyNoteFolderOverwrite"]
         if config["dailyNoteFolderOverwrite"]
-        else "/default/path/to/your/daily/notes"
+        else None
     )
     date_format = (
         config["dailyNoteFilenameOverwrite"]
         if config["dailyNoteFilenameOverwrite"]
-        else "%Y-%m-%d"
+        else None
     )
     list_header_level = config.get("listHeaderLevel", 3)
     section_header = config.get("sectionHeader", "Completed Tasks")
@@ -24,6 +26,26 @@ def write_reminders_to_markdown(reminder_list, completed_reminders):
     time_format = config["timeFormat"]
     separator = config["dateTimeSeparator"]
     wrap_in_link = config.get("wrapDateStringInInternalLink", False)
+    template_path = config["templateOverwritePath"]
+
+    if not folder_path or not date_format:
+        obsidian_parent_path = os.path.dirname(config["obsidianSettingsPath"])
+        periodic_notes_config_path = os.path.join(
+            obsidian_parent_path, "plugins/periodic-notes/data.json"
+        )
+        with open(periodic_notes_config_path, "r") as file:
+            periodic_notes_config = json.load(file)
+            daily_config = periodic_notes_config.get("daily", {})
+            if not folder_path:
+                folder_path = os.path.join(
+                    obsidian_parent_path, daily_config.get("folder")
+                )
+            if not date_format:
+                date_format = daily_config.get("format")
+            if not template_path:
+                template_path = os.path.join(
+                    obsidian_parent_path, daily_config.get("template")
+                )
 
     reminders_by_date = {}
     for reminder in completed_reminders:
@@ -38,14 +60,25 @@ def write_reminders_to_markdown(reminder_list, completed_reminders):
 
     for date, reminders in reminders_by_date.items():
         formatted_filename_date = format_date_for_filename(date, date_format)
-        filename = f"{folder_path}/{formatted_filename_date}.md"
+        filename = os.path.join(folder_path, f"{formatted_filename_date}.md")
         section_header_exists = check_section_header_exists(
             filename, section_header, section_header_level
         )
 
+        if not os.path.exists(filename):
+            with open(filename, "w") as file:
+                if template_path and os.path.exists(template_path):
+                    with open(template_path, "r") as template_file:
+                        file.write(template_file.read())
+                else:
+                    file.write("\n\n")
+        
+        # Append reminders to the daily note
         with open(filename, "a") as file:
             if not section_header_exists:
                 write_section_header(file, section_header, section_header_level)
+            else:
+                file.write("\n")
             append_reminders(
                 file,
                 reminders,
