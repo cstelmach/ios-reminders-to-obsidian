748a4222cadd74dc7b3e4af47735ffcc2caf0383
cstelmach <christian.stel@hotmail.com>
2024-07-20 13:17:58 +0200

    feat: add option for metadata formatting for task tags

--- processing/task_utils.py
+++ processing/task_utils.py
@@ -1,3 +1,13 @@
+from config import config
+
+
+def format_tag(tag):
+    if config.get("useMetadataFormattingForTags", False):
+        return f"(taskTag:: {tag})"
+    return tag
+
+
 def write_task_tags(file, tags, prefix=""):
     if tags:
-        file.write(f"{prefix}\t- tags: {', '.join(tags)}\n")
+        formatted_tags = ", ".join(format_tag(tag) for tag in tags)
+        file.write(f"{prefix}\t- tags: {formatted_tags}\n")
